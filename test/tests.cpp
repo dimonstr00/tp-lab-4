51040b742f742bd64fc76e55da1a24069962b35b
dimonstr00 <dimonstr00@gmail.com>
2020-02-18 14:15:57 +0300

    Vorobyov b77

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,14 +1,2 @@
 #include "gtest/gtest.h"
 #include "Automata.h"
-#include <iostream>
-using namespace std;
-TEST(lab4,task)
-{
-	Automata *a = new Automata();
-	a->on();
-	a->printMenu();
-	a->coin(50);
-	a->choise(1);
-	a->printCash();
-	EXPECT_EQ(5,a->printCash());
-}
