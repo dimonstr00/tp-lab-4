5a0b73c1435769c59507ff6ad737bafd1299bdb6
dimonstr00 <dimonstr00@gmail.com>
2020-02-18 14:25:23 +0300

    test

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,13 @@
 #include "gtest/gtest.h"
 #include "Automata.h"
+
+TEST(lab4, task1) 
+{
+	bool test;
+	Automata *a = new Automata();
+	a->on();
+	a->coin(50);
+	a->choise(1);
+	test=a->check(1);
+	EXPECT_EQ(true, test);
+}
